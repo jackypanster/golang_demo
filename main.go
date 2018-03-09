357f1f538fd58723b708ac25b2704eebf54d052a
zhibinpan <jackypanster@gmail.com>
2018-03-09 23:12:26 +0800

    update

--- main.go
+++ main.go
@@ -4,6 +4,22 @@ import (
 	"log"
 )
 
+type Person struct {
+	Name string
+	Age int
+}
+
+func Test(any interface{}) {
+	p, ok := any.(Person) 
+	if ok {
+		log.Printf("casting works %+v", p)
+	}
+}
+
 func main() {
-	log.Println("main")
+	p := Person{
+		Name: "JP",
+		Age: 37,
+	}
+	Test(p)
 }
